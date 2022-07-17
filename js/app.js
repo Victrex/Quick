d657d1249a04e3903498d52bd07ff0953fb7cd25
Victrex <victrex1234@gmail.com>
2022-07-17 11:38:03 -0600

    agregando la parte de los productos

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ var x = document.getElementById("myTopnav");
     var y = document.getElementById('icon');
     var z = document.getElementById('body');
     var footerActive = document.getElementById('footer_nav');
+
 function navCollapse() {
     
     if (x.className === "topnav") {
@@ -14,3 +15,26 @@ function navCollapse() {
     footerActive.classList.toggle('active');
   }
 
+const changeIcon = () => {
+  
+  let IconList = document.getElementById('listIcon');
+  let corp = document.getElementById('corp');
+  corp.style = '';
+  let cardCorp = document.querySelectorAll('.cardCorp');
+  if (IconList.classList == 'fa-solid fa-list-ul') {
+    IconList.classList = 'fa-solid fa-grip-vertical';
+    cardCorp.forEach(element =>{
+      element.classList = "cardCorp gridCard";
+    });
+    corp.classList = 'corp_container grid'
+
+    
+
+  } else{
+    IconList.classList = 'fa-solid fa-list-ul';
+    cardCorp.forEach(element =>{
+      element.classList = "cardCorp flexCard";
+    });
+    corp.classList = 'corp_container flex'
+  }
+}
