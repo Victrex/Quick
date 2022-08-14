5d7c34a924c6a54ce4bb649a1aefb081f32f4032
victrex <victrex1234@gmail.com>
2022-08-14 16:12:04 -0600

    commit

--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,21 @@ import express, {Express, Response, Request} from "express";
 import mongoose, {connect, Schema} from "mongoose";
 import dotenv from 'dotenv';
 import cors from 'cors';
+import path from "path";
 import adminRoutes from "./routes/admin.routes";
 dotenv.config();
 //configs
 const app: Express = express();
 const port = process.env.PORT;
-<<<<<<< HEAD
+
 
 app.use(cors())
-app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-=======
 var publicPath = path.resolve(__dirname, './src/public');
 app.use(express.json())
 app.use(express.static(path.join(__dirname, 'public')))
->>>>>>> 2b79d39aa171fe6edf023eb234683af10bd04088
+
 
 
 //routes
