1fa5fb46a747a899a7479bb24162bb3e2c535f80
DanielsHndz <16danielshrnandez@gmail.com>
2022-08-14 11:39:25 -0600

    Guardando Cambios

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,22 @@
 import express, {Express, Response, Request} from "express";
 import dotenv from 'dotenv';
+import cors from 'cors';
 import adminRoutes from "./routes/admin.routes";
-import path from "path";
 dotenv.config();
 //configs
 const app: Express = express();
 const port = process.env.PORT;
-var publicPath = path.resolve(__dirname, 'public');
-app.use(express.static(publicPath))
+
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
 
 
 //routes
 app.use('/admin', adminRoutes)
 
 
-
-//
 
 //server
 app.listen(port, () =>{
